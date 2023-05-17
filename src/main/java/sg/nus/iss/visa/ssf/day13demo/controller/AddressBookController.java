fcbb017b20a2a98b09e9c0b2b046f6b1aa591b32
russell258 <duckie258@gmail.com>
2023-05-17 11:53:20 +0800

    conditional validation and error handling with spring boot validation

--- src/main/java/sg/nus/iss/visa/ssf/day13demo/controller/AddressBookController.java
+++ src/main/java/sg/nus/iss/visa/ssf/day13demo/controller/AddressBookController.java
@@ -2,10 +2,16 @@ package sg.nus.iss.visa.ssf.day13demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.validation.Valid;
 import sg.nus.iss.visa.ssf.day13demo.model.Contact;
 
 @Controller
@@ -22,14 +28,20 @@ public class AddressBookController {
 
     ///post with the pathing /contact to be from addressBook.html form action
     //to save the contact information. 
-    @PostMapping("/contact")
-    public String saveAddressBook(Contact contact, Model model){
-        
+    @PostMapping(consumes="application/x-www-form-urlencoded" , path = "/contact")
+    //can also be public String saveAddressBook(HttpServletRequest request, Model model) see day 12. can use requests or object
+    public String saveAddressBook(@Valid Contact contact, BindingResult result, Model model){
         System.out.println("Name: "+contact.getName());
         System.out.println("Email: "+contact.getEmail());
         System.out.println("Phone Number: "+contact.getPhoneNumber());
+    
+     if(result.hasErrors()){
+        System.out.println("error count -->" +result.getErrorCount());
+     return "addressBook";  
+    
+     }
 
         return "addressBook";
     }
-
 }
+
--- src/main/java/sg/nus/iss/visa/ssf/day13demo/model/Contact.java
+++ src/main/java/sg/nus/iss/visa/ssf/day13demo/model/Contact.java
@@ -1,8 +1,21 @@
 package sg.nus.iss.visa.ssf.day13demo.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Contact {
+    
+    @NotNull(message = "Name can not be empty")
+    @Size(min = 3, max = 15, message = "Name should be between 3 to 15 characters")
     private String name;
+
+    @NotEmpty(message = "Email can not be empty")
+    @Email(message = "Invalid email")
     private String email;
+
+    @Size(min = 8, message = "Invalid Phone Number")
     private String phoneNumber;
 
     public Contact() {
